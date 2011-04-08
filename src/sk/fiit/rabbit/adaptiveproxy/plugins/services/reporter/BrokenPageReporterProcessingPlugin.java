69b8399417662043704f0cd50ec94cd0947e94b8
Jan Hudek <hudek.jan@gmail.com>
2011-04-08 17:26:35 +0200

    Add broken pages reporter files

---
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/reporter/BrokenPageReporterProcessingPlugin.java
@@ -0,0 +1,178 @@
+package sk.fiit.rabbit.adaptiveproxy.plugins.services.reporter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import sk.fiit.peweproxy.headers.RequestHeader;
+import sk.fiit.peweproxy.headers.ResponseHeader;
+import sk.fiit.peweproxy.messages.HttpMessageFactory;
+import sk.fiit.peweproxy.messages.HttpResponse;
+import sk.fiit.peweproxy.messages.ModifiableHttpRequest;
+import sk.fiit.peweproxy.messages.ModifiableHttpResponse;
+import sk.fiit.peweproxy.plugins.PluginProperties;
+import sk.fiit.peweproxy.services.ProxyService;
+import sk.fiit.peweproxy.services.content.ModifiableStringService;
+import sk.fiit.peweproxy.services.content.StringContentService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.DatabaseConnectionProviderService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.servicedefinitions.PageInformationProviderService;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.common.SqlUtils;
+import sk.fiit.rabbit.adaptiveproxy.plugins.services.injector.ClientBubbleMenuProcessingPlugin;
+
+
+
+public class BrokenPageReporterProcessingPlugin  extends ClientBubbleMenuProcessingPlugin {
+	
+	String pacGeneratorUrl = "";
+	
+	@Override
+	public HttpResponse getResponse(ModifiableHttpRequest request, HttpMessageFactory messageFactory) {
+	
+		StringContentService stringContentService = request.getServicesHandle().getService(StringContentService.class);
+
+		Map<String, String> postData = getPostDataFromRequest(stringContentService.getContent());
+		String content = "";
+		Connection connection = null;
+		
+		if(postData != null && request.getServicesHandle().isServiceAvailable(DatabaseConnectionProviderService.class)) {				
+			try {
+				connection = request.getServicesHandle().getService(DatabaseConnectionProviderService.class).getDatabaseConnection();
+	
+				if (request.getRequestHeader().getRequestURI().contains("action=reportPage")) {
+					content = this.reportPage(connection, postData.get("uid"), request.getRequestHeader().getField("Referer"));
+				}
+				if (request.getRequestHeader().getRequestURI().contains("action=getReportedStatus")) {
+					content = this.getPageStatus(connection, request.getRequestHeader().getField("Referer"));
+				}
+				
+			} finally {
+				SqlUtils.close(connection);
+			}
+		}
+		
+		ModifiableHttpResponse httpResponse = messageFactory.constructHttpResponse(null, "text/html");
+		ModifiableStringService stringService = httpResponse.getServicesHandle().getService(ModifiableStringService.class);
+		stringService.setContent(content);
+		
+		return httpResponse;
+	}
+	
+	private String reportPage(Connection connection, String uid, String url) {
+		PreparedStatement stmt = null;
+		java.util.Date today = new java.util.Date();
+		String timestamp = new Timestamp(today.getTime()).toString();
+		String formatedTimeStamp = timestamp.substring(0, timestamp.indexOf("."));
+		
+		try {
+			stmt = connection.prepareStatement("INSERT INTO `broken_pages` (`url`, `uid`, `timestamp`) VALUES (?, ?, ?);");
+			
+			stmt.setString(1, url);
+			stmt.setString(2, uid);
+			stmt.setString(3, formatedTimeStamp);
+
+			stmt.execute();
+			
+			URL requestUrl;
+			try {
+				requestUrl = new URL(pacGeneratorUrl);
+				InputStream is = requestUrl.openStream();
+			} catch (MalformedURLException e) {
+				//logger.error("Could send generate pac file request ", e);
+			} catch (IOException e) {
+				//logger.error("Could send generate pac file request ", e);
+			}
+
+			
+			return "OK";
+		} catch (SQLException e) {
+			//logger.error("Could not get messageboard count ", e);
+			return "FAIL";
+		} finally {
+			SqlUtils.close(stmt);
+		}
+	}
+	
+	private String getPageStatus(Connection connection, String url) {
+		PreparedStatement stmt = null;
+		int pageStatus = 0;
+		
+		try {
+			stmt = connection.prepareStatement("SELECT COUNT(`id`) FROM `broken_pages` WHERE `url` = ?;");
+			
+			stmt.setString(1, url);
+			stmt.execute();
+			ResultSet rs = stmt.getResultSet();
+			
+			while (rs.next()) {
+				pageStatus = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			//logger.error("Could not get messageboard count ", e);
+			return "FAIL";
+		} finally {
+			SqlUtils.close(stmt);
+		}
+		
+		if (pageStatus > 0) {
+			return "true";
+		} else {
+			return "false";
+		}
+	}
+		
+	private Map<String, String> getPostDataFromRequest (String requestContent) {
+		try {
+			requestContent = URLDecoder.decode(requestContent, "utf-8");
+		} catch (UnsupportedEncodingException e) {
+			//logger.warn(e);
+		}
+		Map<String, String> postData = new HashMap<String, String>();
+		String attributeName;
+		String attributeValue;
+
+		for (String postPair : requestContent.split("&")) {
+			if (postPair.split("=").length == 2) {
+				attributeName = postPair.split("=")[0];
+				attributeValue = postPair.split("=")[1];
+				postData.put(attributeName, attributeValue);
+			}
+		}
+
+		return postData;
+	}
+	@Override
+	public boolean start (PluginProperties props) {
+		this.pacGeneratorUrl = props.getProperty("pacGeneratorUrl");
+		super.start(props);
+		return true;
+	}
+
+	
+	@Override
+	public void desiredRequestServices (
+			Set<Class<? extends ProxyService>> desiredServices,
+			RequestHeader clientRQHeader) {
+		super.desiredRequestServices(desiredServices, clientRQHeader);
+		desiredServices.add(ModifiableStringService.class); //FIXME: toto je docasny hack kvoli late processingu, spravne tu ma byt len StringContentService
+		desiredServices.add(DatabaseConnectionProviderService.class);
+	}
+	
+	@Override
+	public void desiredResponseServices (
+			Set<Class<? extends ProxyService>> desiredServices,
+			ResponseHeader webRPHeader) {
+		super.desiredResponseServices(desiredServices, webRPHeader);
+		desiredServices.add(PageInformationProviderService.class);
+	}
+}
---
+++ static/peweproxy_reporter.js
@@ -0,0 +1,88 @@
+
+var renewSmallButton = true;
+var reporterButonSelector = 'div#peweproxy_icon_banner a.__peweproxy_reporter_button';
+
+var peweproxy_url_reporter = 'adaptive-proxy/reporter_call.html'
+
+var temp = function($) {
+    $(document).ready(function(){
+    	reportedStatus = false;
+        reportedStatus = getReportedStatus();
+
+        
+        
+        $(reporterButonSelector).click(function(){
+            //$(this).blur();
+            //renewSmallButton = false;
+        	$('#peweproxy_reporter').hide().removeClass('hidden').fadeIn('fast');
+            
+            return false;
+        });
+        $('div#peweproxy_reporter a.__peweproxy_reporter_closebutton').click(function(){
+            $(this).blur();
+            //renewSmallButton = true;
+            $('#peweproxy_reporter').fadeOut('fast');
+            return false;
+        });
+        
+        	
+		$('a#peweproxy_reporter_cancel_button').click(function(){
+            $(this).blur();
+            renewSmallButton = true;
+            
+            $('#peweproxy_reporter').fadeOut('fast');
+            return false;
+        });
+        
+        $('a#peweproxy_reporter_confirm_button').click(function(){            
+            $.post(peweproxy_url_reporter+'?action=reportPage', {
+			  uid: __peweproxy_uid
+			});
+			$('#peweproxy_statusFalse').fadeOut('fast');
+			$('#peweproxy_statusTrue').fadeIn('fast');
+            return false;
+        });
+  
+    });
+/*
+    $(document).scroll(function(){
+        $('#peweproxy_addons_container').animate({
+            'top':$(document).scrollTop()
+        }, 'fast');
+    });
+*/
+} (adaptiveProxyJQuery);
+
+function getReportedStatus(){
+	var retVal;
+    var temp = function($) {
+        retVal = $.ajax({
+            async: false,
+            url: peweproxy_url_reporter+"?action=getReportedStatus",
+            data: {
+                uid: __peweproxy_uid
+            },
+            type: 'POST'
+        }).responseText;
+        
+        if (retVal == "true"){
+        $('#peweproxy_statusFalse').addClass('hidden');
+        
+        	$('#peweproxy_statusFalse').fadeOut('fast');
+			$('#peweproxy_statusTrue').fadeIn('fast');
+        }
+        
+    } (adaptiveProxyJQuery);
+    return retVal;
+}
+
+function setShown(shown){
+    var temp = function($) {
+        shown = shown ? 1 : 0;
+        $.post(peweproxy_url_reporter+'?action=setShown', {
+            shown: shown,
+            uid: __peweproxy_uid
+        });
+
+    } (adaptiveProxyJQuery);
+}
