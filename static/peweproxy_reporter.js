48624f57e57e976b4eff69849a0a285ae7ce181d
Ivan Plesko <ivan.plesko@gmail.com>
2011-05-05 23:26:58 +0200

    Register reporter as peweproxy module

--- static/peweproxy_reporter.js
+++ static/peweproxy_reporter.js
@@ -1,69 +1,64 @@
+peweproxy.register_module('reporer', function($) {
+	
+	var reporterButonSelector = 'div#peweproxy_icon_banner a.__peweproxy_reporter_button';
+	
+	var peweproxy_url_reporter = 'adaptive-proxy/reporter_call.html'
+	
+	
+	$(document).ready(function(){
+	    getReportedStatus();
+		$(reporterButonSelector).click(function(){
+			$('#peweproxy_reporter').hide().removeClass('hidden').fadeIn('fast');
+	              
+			return false;
+		});
+		$('div#peweproxy_reporter a.__peweproxy_reporter_closebutton').click(function(){
+			$(this).blur();
+			$('#peweproxy_reporter').fadeOut('fast');
+			return false;
+		});
+	          
+	          	
+		$('a#peweproxy_reporter_cancel_button').click(function(){
+			$(this).blur();
+			renewSmallButton = true;
+	              
+			$('#peweproxy_reporter').fadeOut('fast');
+			return false;
+		});
+	          
+		$('a#peweproxy_reporter_confirm_button').click(function(){   
+				
+			__ap_register_callback(function(){
+				$.post(peweproxy_url_reporter+'?action=reportPage', {
+					uid: __peweproxy_uid
+				});
+				$('#peweproxy_statusFalse').fadeOut('fast');
+				$('#peweproxy_statusTrue').fadeIn('fast');
+			});
+			return false;
+		});
+	});
+	
+	var getReportedStatus = function(){
+		var retVal;
+		__ap_register_callback(function(){
+			retVal = $.ajax({
+				async: false,
+				url: peweproxy_url_reporter+"?action=getReportedStatus",
+				data: {
+					uid: __peweproxy_uid
+				},
+				type: 'POST'
+			}).responseText;
+	          
+			if (retVal == "true"){
+				$('#peweproxy_statusFalse').addClass('hidden');
+				$('#peweproxy_statusFalse').fadeOut('fast');
+				$('#peweproxy_statusTrue').fadeIn('fast');
+			}
+		});
+		return retVal;
+	}
 
-var renewSmallButton = true;
-var reporterButonSelector = 'div#peweproxy_icon_banner a.__peweproxy_reporter_button';
-
-var peweproxy_url_reporter = 'adaptive-proxy/reporter_call.html'
-
-var temp = function($) {
-    $(document).ready(function(){
-      __ap_register_callback(function(){
-      	reportedStatus = false;
-          reportedStatus = getReportedStatus();
-  
-          
-          
-          $(reporterButonSelector).click(function(){
-          	$('#peweproxy_reporter').hide().removeClass('hidden').fadeIn('fast');
-              
-              return false;
-          });
-          $('div#peweproxy_reporter a.__peweproxy_reporter_closebutton').click(function(){
-              $(this).blur();
-              $('#peweproxy_reporter').fadeOut('fast');
-              return false;
-          });
-          
-          	
-  		$('a#peweproxy_reporter_cancel_button').click(function(){
-              $(this).blur();
-              renewSmallButton = true;
-              
-              $('#peweproxy_reporter').fadeOut('fast');
-              return false;
-          });
-          
-          $('a#peweproxy_reporter_confirm_button').click(function(){            
-              $.post(peweproxy_url_reporter+'?action=reportPage', {
-  			  uid: __peweproxy_uid
-  			});
-  			$('#peweproxy_statusFalse').fadeOut('fast');
-  			$('#peweproxy_statusTrue').fadeIn('fast');
-              return false;
-          });
-    
-      });
-    });
-} (adaptiveProxyJQuery);
-
-function getReportedStatus(){
-	var retVal;
-    var temp = function($) {
-      __ap_register_callback(function(){
-          retVal = $.ajax({
-              async: false,
-              url: peweproxy_url_reporter+"?action=getReportedStatus",
-              data: {
-                  uid: __peweproxy_uid
-              },
-              type: 'POST'
-          }).responseText;
-          
-          if (retVal == "true"){
-            $('#peweproxy_statusFalse').addClass('hidden');
-            $('#peweproxy_statusFalse').fadeOut('fast');
-            $('#peweproxy_statusTrue').fadeIn('fast');
-          }
-      });
-    } (adaptiveProxyJQuery);
-  return retVal;
-}
+});
